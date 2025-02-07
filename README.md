199ae0936cfe2544817482c602c822c837e66131
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-02-07 10:51:08 -1000

    Create directory structure and start pseudocode

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # calculator
 The Odin Project - Calculator
+
+Using what I've learned so far with HTML, CSS, and JavaScript to create a calculator app.
---
+++ scripts.js
@@ -0,0 +1,18 @@
+/*
+
+Create function for adding 2 numbers
+Create function for subtracting 2 numbers
+Create function for multiplying 2 numbers
+Create function for dividing 2 numbers
+
+Create 3 variables to store each part of the operation(number, operator, another number)
+
+Create buttons representing each digit and operator(including =)
+
+Create calculator display
+
+Create clear display button
+
+Create function that populates the display with clicked on digit buttons
+
+*/
