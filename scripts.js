d369399c3d11bb0ca2b24d31a63f5780b79b296e
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-02-07 19:52:13 -1000

    Create operation functions

--- scripts.js
+++ scripts.js
@@ -1,18 +1,21 @@
-/*
+// Create function for adding 2 numbers
+let addition = (num1, num2) => num1 + num2;
 
-Create function for adding 2 numbers
-Create function for subtracting 2 numbers
-Create function for multiplying 2 numbers
-Create function for dividing 2 numbers
+// Create function for subtracting 2 numbers
+let subtraction = (num1, num2) => num1 - num2;
 
-Create 3 variables to store each part of the operation(number, operator, another number)
+// Create function for multiplying 2 numbers
+let multiplication = (num1, num2) => num1 * num2;
 
-Create buttons representing each digit and operator(including =)
+// Create function for dividing 2 numbers
+let division = (num1, num2) => num1 / num2;
 
-Create calculator display
+// Create 3 variables to store each part of the operation(number, operator, another number)
 
-Create clear display button
+// Create buttons representing each digit and operator(including =)
 
-Create function that populates the display with clicked on digit buttons
+// Create calculator display
 
-*/
+// Create clear display button
+
+// Create function that populates the display with clicked on digit buttons
